7c9d2a7c14c40e40a0cf57cd90a9d57dad4dc032
mohamed123 <chaabenim318@gmail.com>
2023-07-08 20:58:31 +0100

    update file

--- src/components/CartMovie.js
+++ src/components/CartMovie.js
@@ -4,7 +4,10 @@ import image from "../images/Film-App-Logo.png";
 import { Link } from "react-router-dom";
 
 const CartMovie = ({ items }) => {
-    return (
+
+    console.log({ CartMovie : items === items._embedded? "_embedded" : "show"  })
+
+    return items._embedded ? (
         <Col xs="6" sm="6" md="4" lg="3" className="my-1">
             <Link to={`/movei/${items.id}`}>
                 <div className="card">
@@ -21,6 +24,27 @@ const CartMovie = ({ items }) => {
                         <p>title : {items._embedded.show.name} </p>
                         <p>language : {items._embedded.show.language} </p>
                         <p>premiered : {items._embedded.show.premiered} </p>
+                    </div>
+                </div>
+            </Link>
+        </Col>
+    ) : (
+        <Col xs="6" sm="6" md="4" lg="3" className="my-1">
+            <Link to={`/movei/${items.id}`}>
+                <div className="card">
+                    <img
+                        src={
+                            items.image !== null
+                                ? items.image.original
+                                : image
+                        }
+                        className="card-image"
+                        alt="image"
+                    />
+                    <div className="card-overlay d-flex flex-column justify-content-center ">
+                        <p>title : {items.name} </p>
+                        <p>language : {items.language} </p>
+                        <p>premiered : {items.premiered} </p>
                     </div>
                 </div>
             </Link>
--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -1,29 +1,23 @@
 import axios from "axios";
 import { ALLMOVIE, SEARCHMOVIE } from "../types/typeMovie";
 
-export const getAllMovies = () => {
-    return async (dispatch) => {
-        try {
-            const res = await axios.get("https://api.tvmaze.com/schedule/full");
-            dispatch({ type: ALLMOVIE, data: res.data });
-            // console.log({ getAllMovies: res.data });  
-        } catch (errer) {
-            console.log(errer);
-        }
-    };
+export const getAllMovies = () => async (dispatch) => {
+    try {
+        const res = await axios.get("https://api.tvmaze.com/schedule/full");
+        dispatch({ type: ALLMOVIE, data: res.data });
+    } catch (errer) {
+        console.log(errer);
+    }
 };
 
-export const searchMovies = (keyword) => {
-    return async (dispatch) => {
-        try {
-            const res = await axios.get(
-                `https://api.tvmaze.com/schedule/full?q=${keyword}`
-            );
-            dispatch({ type: SEARCHMOVIE, data: res });  
-            console.log({ searchMovies : res });
-        } catch (error) {
-            console.log(error);
-        }
-    };
+export const searchMovies = (keyword) => async (dispatch) => {
+    try {
+        const res = await axios.get(
+            `https://api.tvmaze.com/schedule?q=${keyword}`
+        );
+        dispatch({ type: ALLMOVIE, data: res.data.map((item) => item.show) });
+        console.log({ searchMovies: res.data.map((item) => item.show) });
+    } catch (error) {
+        console.log("mathamech data ");
+    }
 };
-
